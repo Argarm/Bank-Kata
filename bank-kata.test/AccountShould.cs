336b39f0a0d805f59619dee4d20fff1f2dde3c11
Aaron García <aargarcia@aidacanarias.com>
2021-09-05 18:56:01 +0100

    red : print_statement_when_have_one_deposit_and_one_withdraw

--- bank-kata.test/AccountShould.cs
+++ bank-kata.test/AccountShould.cs
@@ -16,7 +16,7 @@ namespace bank_kata.test {
         public void print_empty_statement_when_there_not_any_movements() {
             printer = Substitute.For<IPrinter>();
             timeProvider = Substitute.For<ITimeProvider>();
-            transactionStore = Substitute.For<ITransactionStore>(null);
+            transactionStore = new TransactionStore(timeProvider);
             account = new Account(printer, transactionStore);
 
             account.printStatement();
@@ -28,8 +28,9 @@ namespace bank_kata.test {
         public void print_statement_when_a_deposit_has_ocurred() {
             printer = Substitute.For<IPrinter>();
             timeProvider = Substitute.For<ITimeProvider>();
-            transactionStore = Substitute.For<ITransactionStore>(null);
+            transactionStore = new TransactionStore(timeProvider);
             account = new Account(printer, transactionStore);
+            timeProvider.getTodayAsString().Returns("05/09/2021");
 
             account.deposit(100);
             account.printStatement();
@@ -55,5 +56,21 @@ namespace bank_kata.test {
             printer.Received(1).print("05/09/2021 || 100 || 200");
         }
 
+        [Test]
+        public void print_statement_when_have_one_deposit_and_one_withdraw() {
+            printer = Substitute.For<IPrinter>();
+            timeProvider = Substitute.For<ITimeProvider>();
+            transactionStore = new TransactionStore(timeProvider);
+            account = new Account(printer, transactionStore);
+            timeProvider.getTodayAsString().Returns("05/09/2021", "05/09/2021");
+
+            account.deposit(1000);
+            account.withdraw(500);
+            account.printStatement();
+
+            printer.Received(1).print("Date || Amount || Balance");
+            printer.Received(1).print("05/09/2021 || 1000 || 1000");
+            printer.Received(1).print("05/09/2021 || -500 || 500");
+        }
     }
 }
